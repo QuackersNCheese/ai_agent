b0696055f219699667d2fcb5f86c3501e8fdb247
QuackersNCheese <quackersncheese1024@gmail.com>
2026-04-24 09:24:57 -0600

    up thru ch1 L6 Multiple Messages

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 def main():
     print("Hello from ai-agent!")
@@ -9,9 +11,15 @@ def main():
     if api_key is None:
         raise RuntimeError("API Key not found")
     client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages
         )
     
     if response.usage_metadata == None:
